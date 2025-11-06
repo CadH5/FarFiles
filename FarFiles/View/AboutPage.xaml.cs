195c966cb68a52f70205df2e889150b411db67af
JeeWee <jeeweedepee@gmail.com>
2025-11-05 20:06:49 -0600

    In progress

--- FarFiles/View/AboutPage.xaml.cs
+++ FarFiles/View/AboutPage.xaml.cs
@@ -1,11 +1,9 @@
 //JEEWEE
 //using Microsoft.Maui.Controls;
 //using Microsoft.Maui.Controls.Platform;
-#if WINDOWS
 //JEEWEE
 //using Microsoft.UI.Input;
 //using Microsoft.UI.Xaml;
-#endif
 
 namespace FarFiles;
 
@@ -15,43 +13,33 @@ public partial class AboutPage : ContentPage
 	{
 		InitializeComponent();
         lblVersion.Text = $"Version {AppInfo.Current.VersionString}";
+        imgHand.IsVisible = false;
     }
 
-    //JEEWEE
-    //private async void OnLinkTapped(object sender, Microsoft.UI.Input.TappedEventArgs e)
     private async void OnLinkTapped(object sender, object e)
     {
-        var url = "https://www.cadh5.com/farfiles";
+        var lbl = (Label)sender;
+        var url = lbl.Text;
         await Launcher.Default.OpenAsync(url);
     }
 
-    //JEEWEE
-    //private void OnPointerEntered(object sender, Microsoft.UI.Input.PointerEventArgs e)
     private void OnPointerEntered(object sender, object e)
 
     {
-#if WINDOWS
+        // JWdP 20251105 It is almost impossible to change the cursor.
+        // ChatGPT made me try a solution, for which I had to downgrade from .NET 9 to 8
+        // and it costed me two days of work to repair (besides I could still not figure out the solution)
+        // Now, as a weird substitute, I'm using this image becoming visible/invisible
+        // Note: this OnPointEntered/Exited does not work on Android by finger
         var lbl = (Label)sender;
         lbl.TextColor = Colors.Violet;
-
-        //JEEWEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //var platformView = (FrameworkElement)((View)sender).Handler.PlatformView;
-        //var cursor = InputSystemCursor.CreateFromSystemCursor(InputSystemCursorShape.Hand);
-        //platformView.ProtectedCursor = cursor;
-#endif
+        imgHand.IsVisible = true;
     }
 
-    //JEEWEE
-    //private void OnPointerExited(object sender, Microsoft.UI.Input.PointerEventArgs e)
     private void OnPointerExited(object sender, object e)
     {
-#if WINDOWS
         var lbl = (Label)sender;
         lbl.TextColor = Colors.Blue;
-
-        //JEEWEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        //var platformView = (FrameworkElement)((View)sender).Handler.PlatformView;
-        //platformView.ProtectedCursor = null;
-#endif
+        imgHand.IsVisible = false;
     }
 }
